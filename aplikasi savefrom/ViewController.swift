7a12fce4805338c57b616e7b6bccdbfa3260bd6b
Tubagus M Habiburohman <43885961+M1gadir@users.noreply.github.com>
2018-10-19 14:00:32 +0700

    Initial Commit

---
+++ aplikasi savefrom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aplikasi savefrom
+//
+//  Created by My Macbook on 19/10/18.
+//  Copyright © 2018 TB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
